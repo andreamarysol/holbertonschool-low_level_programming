04bfb9b2eb6b3333bcc4e7f9637845183c2e3e8d
andreamarysol <andreadebachier@gmail.com>
2026-02-13 10:46:25 -0400

    task 6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints all single digit numbers of base 10 without using char
- *
- * Return: 0
+ * main - prints all single digit numbers
  */
 int main(void)
 {
 	int n = 0;
 
 	while (n <= 9)
-		putchar(n + 48);
+		putchar(n++ + 48);
 	putchar('\n');
 
 	return (0);
